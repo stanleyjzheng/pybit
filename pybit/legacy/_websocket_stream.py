3433711186b1b85b2267d8088d0a501d4cd1fc20
dextertd <dexterdickinson@protonmail.com>
2023-03-24 15:21:06 +0000

    legacy support: remove redundant code from revived files

--- pybit/legacy/_websocket_stream.py
+++ pybit/legacy/_websocket_stream.py
@@ -6,8 +6,6 @@ import hmac
 import logging
 import re
 import copy
-from uuid import uuid4
-from . import HTTP
 from . import _helpers
 
 
@@ -19,14 +17,8 @@ SUBDOMAIN_MAINNET = "stream"
 DOMAIN_MAIN = "bybit"
 DOMAIN_ALT = "bytick"
 
-INVERSE_PERPETUAL = "Inverse Perp"
-USDT_PERPETUAL = "USDT Perp"
 USDC_PERPETUAL = "USDC Perp"
 USDC_OPTIONS = "USDC Options"
-SPOT = "Spot"
-CONTRACT = "Contract"
-UNIFIED_MARGIN = "Unified Margin"
-COPY_TRADING = "Copy Trading"
 
 
 class _WebSocketManager:
@@ -274,248 +266,6 @@ class _WebSocketManager:
         self.exited = True
 
 
-class _V3WebSocketManager(_WebSocketManager):
-    def __init__(self, ws_name, **kwargs):
-        callback_function = kwargs.pop("callback_function") if \
-            kwargs.get("callback_function") else self._handle_incoming_message
-        super().__init__(callback_function, ws_name, **kwargs)
-
-        self.subscriptions = {}
-
-        self.private_topics = [
-            "user.position.contractAccount",
-            "user.execution.contractAccount",
-            "user.order.contractAccount",
-            "user.wallet.contractAccount",
-            "outboundAccountInfo",
-            "order",
-            "stopOrder",
-            "ticketInfo",
-        ]
-
-        self.symbol_wildcard = "*"
-        self.symbol_separator = "|"
-
-    def subscribe(self, topic, callback, symbol=None):
-        if symbol is None:
-            symbol = []
-        elif type(symbol) == str:
-            symbol = [symbol]
-
-        def prepare_subscription_args(list_of_symbols):
-            """
-            Prepares the topic for subscription by formatting it with the
-            desired symbols.
-            """
-            def get_all_usdt_symbols():
-                query_symbol_response = HTTP().query_symbol()["result"]
-                for symbol_spec in query_symbol_response:
-                    symbol = symbol_spec["name"]
-                    if symbol.endswith("USDT"):
-                        list_of_symbols.append(symbol)
-                return list_of_symbols
-
-            if topic in self.private_topics:
-                # private topics do not support filters
-                return [topic]
-            elif list_of_symbols == self.symbol_wildcard or not list_of_symbols:
-                # different WSS URL support may or may not support the
-                # wildcard; for USDT, we need to manually get all symbols
-                if self.ws_name != USDT_PERPETUAL:
-                    return [topic.format(self.symbol_wildcard)]
-                list_of_symbols = get_all_usdt_symbols()
-
-            topics = []
-            for symbol in list_of_symbols:
-                topics.append(topic.format(symbol))
-            return topics
-
-        subscription_args = prepare_subscription_args(symbol)
-        self._check_callback_directory(subscription_args)
-
-        while not self.is_connected():
-            # Wait until the connection is open before subscribing.
-            time.sleep(0.1)
-
-        req_id = str(uuid4())
-
-        subscription_message = json.dumps({
-                "op": "subscribe",
-                "req_id": req_id,
-                "args": subscription_args
-            })
-        self.ws.send(subscription_message)
-        self.subscriptions[req_id] = subscription_message
-        self._set_callback(topic, callback)
-
-    def _initialise_local_data(self, topic):
-        # Create self.data
-        try:
-            self.data[topic]
-        except KeyError:
-            self.data[topic] = []
-
-    def _process_delta_orderbook(self, message, topic):
-        self._initialise_local_data(topic)
-
-        # Record the initial snapshot.
-        if "snapshot" in message["type"]:
-            self.data[topic] = message["data"]
-            return
-
-        # Make updates according to delta response.
-        book_sides = {"b": message["data"]["b"],
-                      "a": message["data"]["a"]}
-
-        for side, entries in book_sides.items():
-            for entry in entries:
-                # Delete.
-                if float(entry[1]) == 0:
-                    index = _helpers.find_index(
-                        self.data[topic][side], entry, 0)
-                    self.data[topic][side].pop(index)
-                    continue
-
-                # Insert.
-                price_level_exists = \
-                    entry[0] in \
-                    [level[0] for level in self.data[topic][side]]
-                if not price_level_exists:
-                    self.data[topic][side].append(entry)
-                    continue
-
-                # Update.
-                qty_changed = entry[1] != next(
-                    level[1] for level in self.data[topic][side] if
-                    level[0] == entry[0])
-                if price_level_exists and qty_changed:
-                    index = _helpers.find_index(
-                        self.data[topic][side], entry, 0)
-                    self.data[topic][side][index] = entry
-                    continue
-
-    def _process_delta_ticker(self, message, topic):
-        self._initialise_local_data(topic)
-
-        # Record the initial snapshot.
-        if "snapshot" in message["type"]:
-            self.data[topic] = message["data"]
-
-        # Make updates according to delta response.
-        elif "delta" in message["type"]:
-            for key, value in message["data"].items():
-                self.data[topic][key] = value
-
-    def _process_auth_message(self, message):
-        # If we get successful futures auth, notify user
-        if message.get("success") is True:
-            logger.debug(f"Authorization for {self.ws_name} successful.")
-            self.auth = True
-        # If we get unsuccessful auth, notify user.
-        elif message.get("success") is False or message.get("type") == "error":
-            raise Exception(
-                f"Authorization for {self.ws_name} failed. Please check your "
-                f"API keys and restart. Raw error: {message}"
-            )
-
-    def _process_subscription_message(self, message):
-        if message.get("req_id"):
-            sub = self.subscriptions[message["req_id"]]
-        else:
-            # if req_id is not supported, guess that the last subscription
-            # sent was successful
-            sub = json.loads(list(self.subscriptions.items())[0][1])["args"][0]
-
-        # If we get successful futures subscription, notify user
-        if message.get("success") is True:
-            logger.debug(f"Subscription to {sub} successful.")
-        # Futures subscription fail
-        elif message.get("success") is False:
-            response = message["ret_msg"]
-            logger.error("Couldn't subscribe to topic."
-                         f"Error: {response}.")
-            self._pop_callback(sub[0])
-
-    def _process_normal_message(self, message):
-        topic = message["topic"]
-        if "orderbook" in topic:
-            self._process_delta_orderbook(message, topic)
-            callback_data = copy.deepcopy(message)
-            callback_data["type"] = "snapshot"
-            callback_data["data"] = self.data[topic]
-        elif "tickers" in topic:
-            self._process_delta_ticker(message, topic)
-            callback_data = copy.deepcopy(message)
-            callback_data["type"] = "snapshot"
-            callback_data["data"] = self.data[topic]
-        else:
-            callback_data = message
-        callback_function = self._get_callback(topic)
-        callback_function(callback_data)
-
-    def _handle_incoming_message(self, message):
-        def is_auth_message():
-            if message.get("op") == "auth" \
-                    or message.get("type") == "AUTH_RESP":
-                return True
-            else:
-                return False
-
-        def is_subscription_message():
-            if message.get("op") == "subscribe" \
-                    or message.get("type") == "COMMAND_RESP":
-                return True
-            else:
-                return False
-
-        if is_auth_message():
-            self._process_auth_message(message)
-        elif is_subscription_message():
-            self._process_subscription_message(message)
-        else:
-            self._process_normal_message(message)
-
-    def _extract_topic(self, topic_string):
-        """
-        Regex to return the topic without the symbol.
-        """
-        def is_usdc_private_topic():
-            if re.search(r".*\..*\..*\.", topic_string):
-                return True
-
-        if topic_string in self.private_topics or is_usdc_private_topic():
-            return topic_string
-        topic_without_symbol = re.match(r".*(\..*|)(?=\.)", topic_string)
-        return topic_without_symbol[0]
-
-    @staticmethod
-    def _extract_symbol(topic_string):
-        """
-        Regex to return the symbol without the topic.
-        """
-        symbol_without_topic = re.search(r"(?!.*\.)[A-Z*|]*$", topic_string)
-        return symbol_without_topic[0]
-
-    def _check_callback_directory(self, topics):
-        for topic in topics:
-            if topic in self.callback_directory:
-                raise Exception(f"You have already subscribed to this topic: "
-                                f"{topic}")
-
-    def _set_callback(self, topic, callback_function):
-        topic = self._extract_topic(topic)
-
-        self.callback_directory[topic] = callback_function
-
-    def _get_callback(self, topic):
-        topic = self._extract_topic(topic)
-        return self.callback_directory[topic]
-
-    def _pop_callback(self, topic):
-        topic = self._extract_topic(topic)
-        self.callback_directory.pop(topic)
-
-
 class _FuturesWebSocketManager(_WebSocketManager):
     def __init__(self, ws_name, **kwargs):
         callback_function = kwargs.pop("callback_function") if \
@@ -541,23 +291,10 @@ class _FuturesWebSocketManager(_WebSocketManager):
             Prepares the topic for subscription by formatting it with the
             desired symbols.
             """
-            def get_all_usdt_symbols():
-                query_symbol_response = HTTP().query_symbol()["result"]
-                for symbol_spec in query_symbol_response:
-                    symbol = symbol_spec["name"]
-                    if symbol.endswith("USDT"):
-                        list_of_symbols.append(symbol)
-                return list_of_symbols
 
             if topic in self.private_topics:
                 # private topics do not support filters
                 return [topic]
-            elif list_of_symbols == self.symbol_wildcard or not list_of_symbols:
-                # different WSS URL support may or may not support the
-                # wildcard; for USDT, we need to manually get all symbols
-                if self.ws_name != USDT_PERPETUAL:
-                    return [topic.format(self.symbol_wildcard)]
-                list_of_symbols = get_all_usdt_symbols()
 
             topics = []
             for symbol in list_of_symbols:
