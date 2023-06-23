b20e1df1aee2b18565db4431aa398ffa8ea57f0d
dextertd <dexterdickinson@protonmail.com>
2023-06-23 19:04:58 +0100

    add pre_upgrade module

--- pybit/unified_trading.py
+++ pybit/unified_trading.py
@@ -10,6 +10,7 @@ from ._v5_trade import TradeHTTP
 from ._v5_account import AccountHTTP
 from ._v5_asset import AssetHTTP
 from ._v5_position import PositionHTTP
+from ._v5_pre_upgrade import PreUpgradeHTTP
 from ._v5_spot_leverage_token import SpotLeverageHTTP
 from ._v5_spot_margin_trade import SpotMarginTradeHTTP
 from ._v5_user import UserHTTP
@@ -37,6 +38,7 @@ class HTTP(
     AccountHTTP,
     AssetHTTP,
     PositionHTTP,
+    PreUpgradeHTTP,
     SpotLeverageHTTP,
     SpotMarginTradeHTTP,
     UserHTTP,
