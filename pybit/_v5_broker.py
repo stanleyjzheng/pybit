04c365c9907f4a44e72552606fd2a48a1e55932e
dextertd <dexterdickinson@protonmail.com>
2025-01-23 13:36:30 +0000

    deprecate get_broker_earnings; add get_exchange_broker_earnings

--- pybit/_v5_broker.py
+++ pybit/_v5_broker.py
@@ -11,9 +11,28 @@ class BrokerHTTP(_V5HTTPManager):
         Additional information:
             https://bybit-exchange.github.io/docs/v5/broker/earning
         """
+        self.logger.warning(
+            "get_broker_earnings() is deprecated. See get_exchange_broker_earnings().")
+
         return self._submit_request(
             method="GET",
             path=f"{self.endpoint}{Broker.GET_BROKER_EARNINGS}",
+            query=kwargs,
+            auth=True,
+        )
+
+    def get_exchange_broker_earnings(self, **kwargs) -> dict:
+        """
+        Returns:
+            Request results as dictionary.
+
+        Additional information:
+            https://bybit-exchange.github.io/docs/v5/broker/exchange-earning
+        """
+
+        return self._submit_request(
+            method="GET",
+            path=f"{self.endpoint}{Broker.GET_EXCHANGE_BROKER_EARNINGS}",
             query=kwargs,
             auth=True,
         )
